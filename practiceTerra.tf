c242adc5e36ae51ca5e264450f96b037ed218fc8
root <root@ip-172-31-8-174.eu-west-3.compute.internal>
2025-04-30 09:20:19 +0000

    This is an updated terraform code

--- practiceTerra.tf
+++ practiceTerra.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "example" {
 
 variable "ami_id" {
     description = "This is ami id"
-    default = "ami-0987654321"
+    default = "ami-0160e8d70ebc43ee1"
 }
 
 variable "instance_type"{
