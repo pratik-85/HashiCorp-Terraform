ea926642563df01673d2cc35b99794fe9068b106
root <root@ip-172-31-8-174.eu-west-3.compute.internal>
2025-04-30 09:12:48 +0000

    Terraform Code

---
+++ practiceTerra.tf
@@ -0,0 +1,27 @@
+# Basic Syntax of Terraform
+provider "aws" {
+    region = "ap-southeast-1"
+}
+
+resource "aws_instance" "example" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+}
+
+variable "ami_id" {
+    description = "This is ami id"
+    default = "ami-0987654321"
+}
+
+variable "instance_type"{
+    default = "t2.xlarge"
+    description = "This is an instance type"
+}
+
+output "ami_id" {
+    value = aws_instance.example.ami
+}
+
+output "instance_type" {
+  value = aws_instance.example.instance_type
+}
